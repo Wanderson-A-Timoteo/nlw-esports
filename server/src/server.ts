909e3605478860fdc8f137189a51fe2fed4a35a6
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-09-15 20:17:31 -0400

    Add configuração do servidor para typescript

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,9 @@ app.get('/ads', (request, response) => {
     return response.json([
         {id: 1, name: 'Anúncio 01'},
         {id: 2, name: 'Anúncio 02'},
-        {id: 3, name: 'Anúncio 03'}
+        {id: 3, name: 'Anúncio 03'},
+        {id: 4, name: 'Anúncio 04'}
+
     ])
 })
 
