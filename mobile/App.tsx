a1b44104cc17c763a4528afc442295fded97eb0a
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-09-18 10:21:27 -0400

    Mobile - Add bibliotecas, componente Loading e Home

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,34 @@
+import { StatusBar } from 'react-native';
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+
+import { Home } from './src/screens/Home';
 import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
+
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
     <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {/* Se as fontes do projeto (fontsLoaded) estiver carregadas abre a Home caso contrário faz o Loading para carregar as fonts e depois abre a Home */}
+      {fontsLoaded ? <Home /> : <Loading /> }
       
     </Background>
   );
