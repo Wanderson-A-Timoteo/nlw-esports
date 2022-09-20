43f6a0559192567e62ee450ae50c990cba0042a2
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-09-19 20:31:47 -0400

    Server - Add ORM Prisma, Banco de Dados SQLite e criado migration games

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,26 @@ import express from "express";
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+    //const gamedId =  request.params.id;
     return response.json([
         {id: 1, name: 'Anúncio 01'},
         {id: 2, name: 'Anúncio 02'},
         {id: 3, name: 'Anúncio 03'},
         {id: 4, name: 'Anúncio 04'}
-
     ])
 })
 
+app.get('/ads/:id/discord', (request, response) => {
+    //const adId =  request.params.id;
+    return response.json([])
+})
 app.listen(3333)
