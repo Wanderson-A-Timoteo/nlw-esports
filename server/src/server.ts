c57a6d3d34f439e7dc6c1f0f3cca1f7a1d3a9e85
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-09-21 20:17:30 -0400

    Server - Add rota para listar games, ads e discord

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,15 @@ const app = express()
 const prisma = new PrismaClient()
 
 app.get('/games', async (request, response) => {
-    const games = await prisma.game.findMany()
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true,
+                }
+            }
+        }
+    })
 
     return response.json([games]);
 });
@@ -14,18 +22,48 @@ app.post('/ads', (request, response) => {
     return response.status(201).json([]);
 });
 
-app.get('/games/:id/ads', (request, response) => {
-    //const gamedId =  request.params.id;
-    return response.json([
-        {id: 1, name: 'Anúncio 01'},
-        {id: 2, name: 'Anúncio 02'},
-        {id: 3, name: 'Anúncio 03'},
-        {id: 4, name: 'Anúncio 04'}
-    ])
+app.get('/games/:id/ads', async (request, response) => {
+    const gameId =  request.params.id;
+    
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChanel: true,
+            yearsPlaying: true,
+            hourStart: true,
+            hourEnd: true,
+        },
+        where: {
+            gameId,
+        },
+        orderBy: {
+            createAt: 'desc',
+        }
+    })
+    return response.json(ads.map(ad => {
+        return {
+            ...ad,
+            weekDays: ad.weekDays.split(',')
+        }
+    }))
 })
 
-app.get('/ads/:id/discord', (request, response) => {
-    //const adId =  request.params.id;
-    return response.json([])
+app.get('/ads/:id/discord', async (request, response) => {
+    const adId =  request.params.id;
+
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select: {
+            discord: true,
+        },
+        where: {
+            id: adId,
+        }
+    })
+
+    return response.json({
+        discord: ad.discord,
+    })
 })
 app.listen(3333)
