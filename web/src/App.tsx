c326b5f986c81682cebbe49d8f28b4766bb70c8e
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-09-25 11:35:56 -0400

    Mobile - Efetuado troca de parametros pra buscar dados da API e iniciado criação da pg de anúncio

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -12,25 +12,23 @@ import { Input } from './components/Form/input';
 
 interface Game {
   id: string;
-  
-    title: string;
-
-    bannerUrl: string;
-  _count:{
+  title: string;
+  bannerUrl: string;
+  _count: {
     ads: number;
   }
 }
 
 function App() {
-  const [games, setGames] = useState<Game[]>([])
+  const [games, setGames] = useState<Game[]>([]);
 
   useEffect( () => {
     fetch('http://localhost:3333/games')
-      .then(response => response.json() )
+      .then(response => response.json())
       .then(data => {
-        setGames(data)
-      })
-  }, [])
+        setGames(data);
+      });
+  }, []);
 
   return(
     <div className='max-w-[1344] mx-auto flex flex-col items-center my-20'>
@@ -40,22 +38,18 @@ function App() {
         Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui
       </h1>
       
-      {/* <div className='grid grid-cols-6 gap-6 mt-16'>
+      <div className='grid grid-cols-6 gap-6 mt-16'>
         {games.map(game => {
           return (
             <GameBanner 
               key={game.id}
-              
-                title={game.
-  
-                  title} 
-       
-                  bannerUrl={game.bannerUrl} 
+              title={game.title}
+              bannerUrl={game.bannerUrl} 
               adsCount={game._count.ads}
             />
           )
         })}
-      </div> */}
+      </div>
       
       {/* @Radix-dialog */}
       <Dialog.Root>  
